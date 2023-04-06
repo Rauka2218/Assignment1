55be56598ea36b4d2122dc47acc5456d1061c5e1
Acer Home <klimrayko@gmail.com>
2023-04-06 16:23:38 +0600

    This is the power calculator

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,17 @@
-import java.util.Scanner;
-
-public class Main {
+import java.util.Scanner;public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
         int n = input.nextInt();
-        int i =(int)Math.sqrt(n);
-        if(isPrime(n,i)){
-            System.out.println("YES");
-        }
-        else {
-            System.out.println("NO");
-        }
-
-
-
-
-
-
+        System.out.println(power(a,n));
     }
-    public static boolean isPrime(int n, int i){
-        if(i <= 1 ){
-            return true;
-
-        } else if (n%i==0) {
-            return false;
+    public static int power(int a, int n){
+        if(a==0 || a == 1 || n == 1 ){
+            return a;
+        } else if (n == 0) {
+            return 1;
         }
-        return isPrime(n,i-1);
+            return a*power(a,n-1);
     }
 
 }
